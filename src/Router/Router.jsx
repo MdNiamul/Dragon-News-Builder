106f622dfeb5b7aa926a5fe0c858c2b4f3cc6170
Md Niamul Hossain <niamulhossen223@gmail.com>
2026-03-05 16:13:28 +0600

    create-router

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import { createBrowserRouter } from "react-router";
 
-const Router = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <h2>Home</h2>
+    },
+    {
+        path: '/auth',
+        element: <h2>Authinication here </h2>
+    },
+    {
+        path: '/news',
+        element: <h2>news Layout</h2>
+    },
+    {
+        path: '/*',
+        element: <h2>Error</h2>
+    }
+])
 
-export default Router;
+export default router;
