9db6da1a2cb4b4df8c93c1c51d2f994023036b31
Md Niamul Hossain <niamulhossen223@gmail.com>
2026-03-08 20:39:01 +0600

    customize-provider

--- src/AuthProvide/AuthProvider.jsx
+++ src/AuthProvide/AuthProvider.jsx
@@ -1,12 +1,24 @@
 import React, { Children, createContext, useState } from 'react';
 export const AuthContext = createContext();
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from '../Firebase/Firebase.config';
+
+
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
 
+    const createUser = (email, password)=>{
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
     const authData = {
         user,
         setUser,
+        createUser,
     };
 
     return <AuthContext value={authData}>{children}</AuthContext>
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../AuthProvide/AuthProvider';
 
 const Register = () => {
 
     const handleregisterChange = (e) => {
         e.preventDefault();
+
+        const {createuser} = use(AuthContext);
 
         console.log(e.target);
         const form = e.target;
@@ -14,9 +17,21 @@ const Register = () => {
         const photo = form.photo.value;
         const password = form.password.value;
 
-        console.log(name, email, photo, password);
+        console.log({name, email, photo, password});
+
+        createuser(email, password)
+        .then(result=>{
+            const user = result.user;
+            console.log(user);
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessege = error.message;
+            alert(errorMessege);
+        });
         
-    }
+    };
+    
     return (
         <div className="flex justify-center items-center min-h-screen">
             <div className="card bg-base-100 w-full max-w-sm shadow-2xl py-5">
@@ -27,25 +42,25 @@ const Register = () => {
 
                         {/* name */}
                         <label className="label">Name</label>
-                        <input type="text" name='name' className="input" placeholder="Name" />
+                        <input required type="text" name='name' className="input" placeholder="Name" />
 
                         {/* Photo URl */}
                         <label className="label">Photo URL</label>
-                        <input type="text" name='photo' className="input" placeholder="Photo URL" />
+                        <input required type="text" name='photo' className="input" placeholder="Photo URL" />
 
                         {/* email */}
                         <label className="label">Email</label>
-                        <input type="email" name='email' className="input" placeholder="Email" />
+                        <input required type="email" name='email' className="input" placeholder="Email" />
 
                         {/* password */}
                         <label className="label">Password</label>
-                        <input type="password" name='password' className="input" placeholder="Password" />
+                        <input required type="password" name='password' className="input" placeholder="Password" />
 
                         {/* <div>
                             <a className="link link-hover">Forgot password?</a>
                         </div> */}
 
-                        <button type='submit' className="btn btn-neutral mt-4">Register</button>
+                        <button required type='submit' className="btn btn-neutral mt-4">Register</button>
 
                         <h2 className='font-semibold text-center p-6'>Already Have An Account ? <Link to='/auth/login' className='text-red-700'>Please Login</Link></h2>
                     </fieldset>
