5a0acbf599c51f2a556f01b916b0251c029f10d7
Md Niamul Hossain <niamulhossen223@gmail.com>
2026-03-09 13:23:29 +0600

    u[tade-signout

--- src/AuthProvide/AuthProvider.jsx
+++ src/AuthProvide/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import React, { Children, createContext, useEffect, useState } from 'react';
-export const AuthContext = createContext();
+import React, { createContext, useEffect, useState } from 'react';
+
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../Firebase/Firebase.config';
 
@@ -7,6 +7,9 @@ import app from '../Firebase/Firebase.config';
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
+
+// eslint-disable-next-line
+export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
@@ -17,11 +20,11 @@ const AuthProvider = ({ children }) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
-    const logOut =()=>{
+    const logOut = () => {
         return signOut(auth);
     }
 
-    const signIn = ( email, password) => {
+    const signIn = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
 
@@ -34,6 +37,16 @@ const AuthProvider = ({ children }) => {
         }
     }, [])
 
+    // const authData = {
+    //     user,
+    //     setUser,
+    //     createUser,
+    //     logOut,
+    //     signIn
+    // };
+
+    // return <AuthContext value={authData}>{children}</AuthContext>
+
     const authData = {
         user,
         setUser,
@@ -42,7 +55,11 @@ const AuthProvider = ({ children }) => {
         signIn
     };
 
-    return <AuthContext value={authData}>{children}</AuthContext>
+    return (
+        <AuthContext.Provider value={authData}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
 export default AuthProvider;
