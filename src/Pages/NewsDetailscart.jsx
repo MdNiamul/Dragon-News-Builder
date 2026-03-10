2ab87f17c37912c63ca14ff8266f3a98572d8c51
Md Niamul Hossain <niamulhossen223@gmail.com>
2026-03-10 17:11:21 +0600

    create-button-back

--- src/Pages/NewsDetailscart.jsx
+++ src/Pages/NewsDetailscart.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const NewsDetailscart = ({news}) => {
    console.log(news);
 
     return (
-        <div>
-            <img className='mb-4' src={news.image_url} alt="" />
+        <div className='space-y-4'>
+            <img className='w-full h-[350px] rounded-2xl object-cover mb-4' src={news.image_url} alt="" />
             <h1 className='text-2xl mb-4'>{news.title}</h1>
+            <h2>{news.details}</h2>
+            <Link className='btn btn-secondary ' to={`/catagory/${news.category_id}`}>back to category</Link>
         </div>
     );
 };
