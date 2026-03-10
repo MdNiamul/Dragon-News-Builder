7e58daa8955f302fbbafc83dbdb0f525b62db0dc
Md Niamul Hossain <niamulhossen223@gmail.com>
2026-03-10 16:25:43 +0600

    uptade-details-page

--- src/Pages/News-Details.jsx
+++ src/Pages/News-Details.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Header from '../Component/Header/Header';
+import RightAside from '../Component/RightAside/RightAside';
 
 const NewsDetails = () => {
     return (
         <div>
-            <h2>hi baby </h2>
+            <header className='py-3'>
+                <Header></Header>
+            </header>
+            <main className='w-11/12 mx-auto grid grid-cols-12 py-3 '>
+                <section className='col-span-9'>
+                    <h2 className='font-bold mb-5'>News Details</h2>
+                </section>
+                <aside className='col-span-3'>
+                    <RightAside></RightAside>
+                </aside>
+            </main>
         </div>
     );
 };
