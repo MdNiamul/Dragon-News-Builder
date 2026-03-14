cbd6e594d6e2ffb291bf0b9e5eda32a8e911ca67
Md Niamul Hossain <niamulhossen223@gmail.com>
2026-03-14 09:41:41 +0600

    uptade-finds-zone

--- src/Component/RightAside/FindUs.jsx
+++ src/Component/RightAside/FindUs.jsx
@@ -5,12 +5,39 @@ const FindUs = () => {
     return (
         <div>
             <h2 className='font-bold mb-5'>Find Us On</h2>
-            <div>
-                <div className="join join-vertical w-full">
-                    <button className="btn bg-base-100 join-item justify-start"> <FaFacebook></FaFacebook> facebook</button>
-                    <button className="btn bg-base-100  join-item justify-start"><FaInstagram></FaInstagram> InstraGram</button>
-                    <button className="btn bg-base-100  join-item justify-start"><FaTwitter></FaTwitter> witter</button>
-                </div>
+
+            <div className="join join-vertical w-full">
+
+                <a
+                    href="https://web.facebook.com/niamul.islam.480597"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn bg-base-100 join-item justify-start"
+                >
+                    <FaFacebook className='mr-2' />
+                    Facebook
+                </a>
+
+                <a
+                    href="https://www.instagram.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn bg-base-100 join-item justify-start"
+                >
+                    <FaInstagram className='mr-2' />
+                    Instagram
+                </a>
+
+                <a
+                    href="https://twitter.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn bg-base-100 join-item justify-start"
+                >
+                    <FaTwitter className='mr-2' />
+                    Twitter
+                </a>
+
             </div>
         </div>
     );
